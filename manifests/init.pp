52faad1792df05f3c39cc41fc5731b5e42667ff9
Jon Pascoe <jon.pascoe@me.com>
2013-03-28 15:42:51 +0000

    Fix typo with capitalization

--- manifests/init.pp
+++ manifests/init.pp
@@ -5,7 +5,7 @@
 #   include alfred
 class alfred {
   package { 'Alfred':
+    source	 => 'http://cachefly.alfredapp.com/Alfred_2.0.2_178.zip',
     provider => 'compressed_app',
-    source   => 'http://cachefly.alfredapp.com/alfred_2.0.2_178.zip'
   }
 }
--- spec/classes/alfred_spec.rb
+++ spec/classes/alfred_spec.rb
@@ -4,7 +4,7 @@ describe 'alfred' do
   it do
     should contain_package('Alfred').with({
       :provider => 'compressed_app',
-      :source   => 'http://cachefly.alfredapp.com/alfred_2.0.1_261.dmg',
+      :source   => 'http://cachefly.alfredapp.com/Alfred_2.0.1_261.dmg',
     })
   end
 end
